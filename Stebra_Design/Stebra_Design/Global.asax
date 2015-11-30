5f39d5b4440d5947e7cc91316a9c89576979045e
Peter Ayvazian <peter.ayvazian@outlook.com>
2015-11-30 16:48:02 +0100

    Home page, About page. Mostly copy paste

---
+++ Stebra_Design/Stebra_Design/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Stebra_Design.MvcApplication" Language="C#" %>
---
+++ Stebra_Design/Stebra_Design/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Stebra_Design.Startup))]
+namespace Stebra_Design
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
