1cf53810b51956fd487905ba571ec8285137cee1
Peter Ayvazian <peter.ayvazian@outlook.com>
2015-12-02 16:30:23 +0100

    added newsflash, app.js, actionresult

--- Stebra_Design/Stebra_Design/Controllers/HomeController.cs
+++ Stebra_Design/Stebra_Design/Controllers/HomeController.cs
@@ -26,5 +26,89 @@ namespace Stebra_Design.Controllers
 
             return View();
         }
+
+        public ActionResult NewsFlash()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult News()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult NewsOne()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult NewsTwo()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult NewsThree()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Blogs()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+
+        }
+        public ActionResult BlogsOne()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult BlogsTwo()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult BlogsThree()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Projects()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult ProjectsOne()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult ProjectsTwo()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult ProjectsThree()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
     }
 }
