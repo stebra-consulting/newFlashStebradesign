305da87dc416ffb785023aa4c8fd4b2ac305c806
Peter Ayvazian <peter.ayvazian@outlook.com>
2015-12-02 16:01:47 +0100

    responsive design & added newsflash design

--- Stebra_Design/Stebra_Design/Scripts/Design.js
+++ Stebra_Design/Stebra_Design/Scripts/Design.js
@@ -2,4 +2,12 @@
 
 $(document).ready(function () {
     $('.carousel').carousel();
+
+    var heights = $(".mydiv").map(function () {
+        return $(this).height();
+    }).get(),
+
+    maxHeight = Math.max.apply(null, heights);
+
+    $(".mydiv").height(maxHeight);
 });
