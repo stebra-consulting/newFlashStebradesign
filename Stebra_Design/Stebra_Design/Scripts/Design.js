df5cbea1155ca45f771309216f6b394b476ab2fc
Peter Ayvazian <peter.ayvazian@outlook.com>
2015-12-07 16:07:05 +0100

    stuff

--- Stebra_Design/Stebra_Design/Scripts/Design.js
+++ Stebra_Design/Stebra_Design/Scripts/Design.js
@@ -3,8 +3,7 @@
 $(document).ready(function () {
     $('.carousel').carousel();
 
-    $('.nav a').on('click', function () {
-        $('.btn-navbar').click(); //bootstrap 2.x
+    $('.navbar-collapse a').on('click', function () {
         $('.navbar-toggle').click() //bootstrap 3.x by Richard
     });
 
